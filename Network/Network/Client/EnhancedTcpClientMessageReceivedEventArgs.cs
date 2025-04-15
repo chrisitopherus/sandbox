0748ce1989e20b03b89d50fcacf7ca575428d748
chrisitopherus <jochchristopher@gmail.com>
2025-04-15 20:47:48 +0200

    added event args as base class

---
+++ Network/Network/Client/EnhancedTcpClientMessageReceivedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network.Client;
+
+public class EnhancedTcpClientMessageReceivedEventArgs<TMessage> : EventArgs
+{
+    public EnhancedTcpClientMessageReceivedEventArgs(TMessage message)
+    {
+        this.Message = message;
+    }
+
+    public TMessage Message { get; }
+}
