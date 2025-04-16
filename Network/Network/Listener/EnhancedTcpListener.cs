6bef600b9d6a4039c21d6815c70b074f0997d375
chrisitopherus <jochchristopher@gmail.com>
2025-04-16 19:40:20 +0200

    implemented the listener

--- Network/Network/Listener/EnhancedTcpListener.cs
+++ Network/Network/Listener/EnhancedTcpListener.cs
@@ -1,7 +1,11 @@
-﻿using Network.Util;
+﻿using Network.Architecture;
+using Network.Client;
+using Network.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,25 +13,69 @@ namespace Network.Listener;
 
 public class EnhancedTcpListener<TMessage> : LifecycleComponent
 {
+    private TcpListener listener;
     private readonly EnhancedTcpListenerConfiguration<TMessage> configuration;
+    private CancellationTokenSource? cancellationTokenSource;
     public EnhancedTcpListener(EnhancedTcpListenerConfiguration<TMessage> configuration)
     {
+        this.listener = new TcpListener(configuration.EndPoint);
         this.configuration = configuration;
+        this.State = LifecycleState.Initialized;
     }
 
     public event EventHandler<EnhancedTcpListenerNewClientEventArgs<TMessage>>? NewClient;
+
     public override void Start()
     {
-        throw new NotImplementedException();
+        if (this.State == LifecycleState.Started)
+        {
+            throw new InvalidOperationException("Listener was already started.");
+        }
+
+        this.State = LifecycleState.Started;
+        this.cancellationTokenSource = new CancellationTokenSource();
+        Task _ = Task.Run(() => this.ListenForConnectionsAsync(this.cancellationTokenSource.Token));
     }
 
     public override void Stop()
     {
-        throw new NotImplementedException();
+        if (this.State != LifecycleState.Started)
+        {
+            throw new InvalidOperationException("Listener is not running.");
+        }
+
+        this.cancellationTokenSource?.Cancel();
+        this.cancellationTokenSource = null;
     }
 
     protected virtual void FireOnNewClient(EnhancedTcpListenerNewClientEventArgs<TMessage> e)
     {
         this.NewClient?.Invoke(this, e);
+    }
+
+    private async Task ListenForConnectionsAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                this.listener.Start();
+                TcpClient tcpClient = await this.listener.AcceptTcpClientAsync(cancellationToken);
+                EnhancedTcpClient<TMessage> client = new EnhancedTcpClient<TMessage>(tcpClient, this.configuration.ClientConfiguration);
+                this.FireOnNewClient(new EnhancedTcpListenerNewClientEventArgs<TMessage>(client));
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // Expected
+        }
+        catch
+        {
+            // Exception Handling
+        }
+        finally
+        {
+            this.State = LifecycleState.Stopped;
+        }
     }
 }
