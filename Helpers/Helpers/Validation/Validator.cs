85b7871c5657856d60ef771933c971b7a03c7c92
chrisitopherus <jochchristopher@gmail.com>
2025-04-17 18:33:26 +0200

    exported the validator class to its own solution

--- Helpers/Helpers/Validation/Validator.cs
+++ Helpers/Helpers/Validation/Validator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Network.Util;
+﻿namespace Helpers.Validation;
 
 public static class Validator
 {
