a7204e8cecf69cbef808df07939ef98c53fda90f
chrisitopherus <jochchristopher@gmail.com>
2025-04-18 19:49:42 +0200

    added utility class

---
+++ Helpers/Helpers/Utility/ConsoleSettings.cs
@@ -0,0 +1,36 @@
+﻿using Helpers.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility;
+
+public class ConsoleSettings
+{
+    private int windowWidth;
+    private int windowHeight;
+    private int bufferWidth;
+    private int bufferHeight;
+    private ConsoleStyle style;
+
+    public int WindowWidth
+    {
+        get
+        {
+            return this.windowWidth;
+        }
+
+        private set
+        {
+            Validator.NotWithin(value, 1, Console.LargestWindowWidth, nameof(this.WindowWidth));
+            this.windowWidth = value;
+        }
+    }  
+
+    public static ConsoleSettings Capture()
+    {
+        return new ConsoleSettings();
+    }
+}
---
+++ Helpers/Helpers/Utility/ConsoleStyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility;
+
+public struct ConsoleStyle
+{
+    public ConsoleStyle(ConsoleColor foreground, ConsoleColor background)
+    {
+        this.Foreground = foreground;
+        this.Background = background;
+    }
+
+    public ConsoleColor Foreground { get; init; }
+    public ConsoleColor Background { get; init; }
+}
