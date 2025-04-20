9fae451c9de423d7c99078154c830c689d68fdfe
chrisitopherus <jochchristopher@gmail.com>
2025-04-20 21:37:54 +0200

    added collision directory

---
+++ Demos/ConsoleWyrm/Server/CollisionDetection/CollisionDetector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Server.CollisionDetection;
+
+public class CollisionDetector
+{
+}
