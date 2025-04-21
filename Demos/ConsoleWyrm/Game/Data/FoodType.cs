7fe48fdb51660067f5b1c69782d947c086319ea2
chrisitopherus <jochchristopher@gmail.com>
2025-04-21 18:21:44 +0200

    added basic classes for game

---
+++ Demos/ConsoleWyrm/Game/Data/FoodType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Game.Data;
+
+// food like steroid(red), pickle(green), esmacho(either rgb or blue)
+public enum FoodType
+{
+    Steroid,
+    Pickle,
+    Esmacho
+}
