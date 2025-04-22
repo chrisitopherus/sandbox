60a924dda7f19380593121959b439f5df9dff37f
chrisitopherus <jochchristopher@gmail.com>
2025-04-22 19:35:12 +0200

    added codec registry

---
+++ Demos/ConsoleWyrm/Utility/MessageCodecRegistry.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleWyrm.Networking.Messages.Data;
+using Network.Architecture.Interfaces.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Utility;
+
+public class MessageCodecRegistry<TMessage>
+{
+    private readonly Dictionary<MessageType, IMessageCodec<TMessage>> codecs = [];
+
+    public MessageCodecRegistry<TMessage> Register(MessageType type, IMessageCodec<TMessage> codec)
+    {
+        this.codecs.Add(type, codec);
+        return this;
+    }
+
+    public IMessageCodec<TMessage> GetValue(MessageType type)
+    {
+        IMessageCodec<TMessage> value;
+        if (!this.TryGetValue(type, out value))
+        {
+            throw new ArgumentOutOfRangeException($"No registered codec for type: {type}");
+        }
+
+        return value;
+    }
+
+    public bool TryGetValue(MessageType type, out IMessageCodec<TMessage> codec)
+    {
+        try
+        {
+            codec = this.codecs[type];
+            return true;
+        }
+        catch
+        {
+            codec = default!;
+            return false;
+        }
+    }
+}
