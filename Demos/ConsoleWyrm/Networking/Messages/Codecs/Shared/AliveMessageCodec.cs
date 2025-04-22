d238e04bdcc0fac8f2d733f29668898dc5cbf352
chrisitopherus <jochchristopher@gmail.com>
2025-04-22 19:35:53 +0200

    changed alivemessage to non static

---
+++ Demos/ConsoleWyrm/Networking/Messages/Codecs/Shared/AliveMessageCodec.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleWyrm.Networking.Messages.Shared;
+using Network.Architecture.Interfaces.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Networking.Messages.Codecs.Shared;
+
+public class AliveMessageCodec : IMessageCodec<AliveMessage>
+{
+    public AliveMessage Decode(ReadOnlyMemory<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadOnlyMemory<byte> Encode(AliveMessage message)
+    {
+        throw new NotImplementedException();
+    }
+}
