31a3ac4b219d1d9be9e03a4960992d376be74056
chrisitopherus <jochchristopher@gmail.com>
2025-04-22 19:34:37 +0200

    added utility

---
+++ Helpers/Helpers/Utility/Registry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility;
+
+public static class Registry<TKey, TValue> where TKey : notnull
+{
+    private static readonly Dictionary<TKey, TValue> items = [];
+
+    public static void Register(TKey key, TValue value)
+    {
+        items.Add(key, value);
+    }
+
+    public static TValue GetValue(TKey key)
+    {
+        TValue value;
+        if (!TryGetValue(key, out value))
+        {
+            throw new ArgumentOutOfRangeException($"No item found with specified key: {key}");
+        }
+
+        return value;
+    }
+
+    public static bool TryGetValue(TKey key, out TValue value)
+    {
+        try
+        {
+            value = items[key];
+            return true;
+        }
+        catch
+        {
+            value = default!;
+            return false;
+        }
+    }
+}
---
+++ Helpers/Helpers/Utility/SpanReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility;
+
+public static class SpanReader
+{
+    public static byte ReadByte(ref ReadOnlySpan<byte> span)
+    {
+        byte value = span[0];
+        span = span[1..];
+        return value;
+    }
+}
