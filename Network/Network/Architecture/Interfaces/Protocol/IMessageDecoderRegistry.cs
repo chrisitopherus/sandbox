c4c3f997c6ec3f5e936c92d33e4d9443bce22668
chrisitopherus <jochchristopher@gmail.com>
2025-04-23 18:56:37 +0200

    added decoder registry interface

---
+++ Network/Network/Architecture/Interfaces/Protocol/IMessageDecoderRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network.Architecture.Interfaces.Protocol;
+
+public interface IMessageDecoderRegistry<TKey, TMessage>
+    where TMessage : IMessage
+{
+    IMessageDecoder<TMessage> GetMessageDecoder(TKey key);
+    bool TryGetMessageDecoder(TKey key, out IMessageDecoder<TMessage> decoder);
+}
