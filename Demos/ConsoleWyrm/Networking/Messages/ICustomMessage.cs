1aad02bfeab720ef5e4b865fde8e24f6b48cca24
chrisitopherus <jochchristopher@gmail.com>
2025-04-23 19:39:04 +0200

    adapted network architecture change -> renamed interface

--- Demos/ConsoleWyrm/Networking/Messages/ICustomMessage.cs
+++ Demos/ConsoleWyrm/Networking/Messages/ICustomMessage.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleWyrm.Networking.Messages.Data;
+using Network.Architecture.Interfaces;
 
 namespace ConsoleWyrm.Networking.Messages;
 
-public interface IMessage
+public interface ICustomMessage : IMessage
 {
     MessageType Type { get; }
 }
 
-public interface IMessage<TVisitor> : IMessage
+public interface ICustomMessage<TVisitor> : ICustomMessage
 {
     void Accept(TVisitor visitor);
 }
