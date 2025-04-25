c09eb2214d79ed2f0d5f24a4567ee79d5e1fbe1d
chrisitopherus <jochchristopher@gmail.com>
2025-04-25 18:13:09 +0200

    added doc

--- Helpers/Helpers/Validation/Validator.cs
+++ Helpers/Helpers/Validation/Validator.cs
@@ -1,7 +1,18 @@
 ﻿namespace Helpers.Validation;
 
+/// <summary>
+/// Provides utility methods for validating method parameters and throwing descriptive exceptions when validation fails.
+/// </summary>
 public static class Validator
 {
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException"/> if the specified value is <see langword="null"/>.
+    /// </summary>
+    /// <typeparam name="TNullable">The type of the nullable value to validate.</typeparam>
+    /// <param name="value">The value to check for null.</param>
+    /// <param name="parameterName">The name of the parameter being validated.</param>
+    /// <param name="message">An optional custom error message. If not provided, a default message is used.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
     public static void NotNull<TNullable>(TNullable value, string parameterName, string? message = null)
     {
         if (message == null)
@@ -15,6 +26,15 @@ public static class Validator
         }
     }
 
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException"/> if the specified value is less than the minimum allowed value.
+    /// </summary>
+    /// <typeparam name="TComparable">The type of the value to validate, which must implement <see cref="IComparable"/>.</typeparam>
+    /// <param name="value">The value to validate.</param>
+    /// <param name="minValue">The minimum allowed value (inclusive).</param>
+    /// <param name="parameterName">The name of the parameter being validated.</param>
+    /// <param name="message">An optional custom error message. If not provided, a default message is used.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is less than <paramref name="minValue"/>.</exception>
     public static void NotLessThan<TComparable>(TComparable value, TComparable minValue, string parameterName, string? message = null)
         where TComparable : IComparable
     {
@@ -29,6 +49,15 @@ public static class Validator
         }
     }
 
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException"/> if the specified value is greater than the maximum allowed value.
+    /// </summary>
+    /// <typeparam name="TComparable">The type of the value to validate, which must implement <see cref="IComparable"/>.</typeparam>
+    /// <param name="value">The value to validate.</param>
+    /// <param name="maxValue">The maximum allowed value (inclusive).</param>
+    /// <param name="parameterName">The name of the parameter being validated.</param>
+    /// <param name="message">An optional custom error message. If not provided, a default message is used.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is greater than <paramref name="maxValue"/>.</exception>
     public static void NotGreaterThan<TComparable>(TComparable value, TComparable maxValue, string parameterName, string? message = null)
         where TComparable : IComparable
     {
@@ -43,6 +72,18 @@ public static class Validator
         }
     }
 
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException"/> if the specified value is outside the inclusive range defined by <paramref name="minValue"/> and <paramref name="maxValue"/>.
+    /// </summary>
+    /// <typeparam name="TComparable">The type of the value to validate, which must implement <see cref="IComparable"/>.</typeparam>
+    /// <param name="value">The value to validate.</param>
+    /// <param name="minValue">The minimum allowed value (inclusive).</param>
+    /// <param name="maxValue">The maximum allowed value (inclusive).</param>
+    /// <param name="parameterName">The name of the parameter being validated.</param>
+    /// <param name="message">An optional custom error message. If not provided, a default message is used.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if <paramref name="value"/> is less than <paramref name="minValue"/> or greater than <paramref name="maxValue"/>.
+    /// </exception>
     public static void NotWithin<TComparable>(TComparable value, TComparable minValue, TComparable maxValue, string parameterName, string? message = null)
         where TComparable : IComparable
     {
