d506a57f04247f42dc94b04b2a193d4ff842c158
chrisitopherus <jochchristopher@gmail.com>
2025-04-25 21:38:07 +0200

    updated span reader and added spanwriter

--- Helpers/Helpers/Utility/Span/SpanReader.cs
+++ Helpers/Helpers/Utility/Span/SpanReader.cs
@@ -2,6 +2,7 @@
 using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,13 @@ public static class SpanReader
         return value;
     }
 
+    public static short ReadShort(ref ReadOnlySpan<byte> span)
+    {
+        short value = BinaryPrimitives.ReadInt16LittleEndian(span);
+        span = span[2..];
+        return value;
+    }
+
     public static int ReadInt(ref ReadOnlySpan<byte> span)
     {
         int value = BinaryPrimitives.ReadInt32LittleEndian(span);
@@ -26,5 +34,24 @@ public static class SpanReader
     public static void Skip(ref ReadOnlySpan<byte> span, int skipAmount)
     {
         span = span[skipAmount..];
+    }
+
+    public static string ReadString(ref ReadOnlySpan<byte> span, int length)
+    {
+        string value = Encoding.UTF8.GetString(span[..length]);
+        span = span[length..];
+        return value;
+    }
+
+    public static string ReadShortLengthPrefixedString(ref ReadOnlySpan<byte> span)
+    {
+        short length = ReadShort(ref span);
+        return ReadString(ref span, length);
+    }
+
+    public static string ReadIntLengthPrefixedString(ref ReadOnlySpan<byte> span)
+    {
+        int length = ReadInt(ref span);
+        return ReadString(ref span, length);
     }
 }
---
+++ Helpers/Helpers/Utility/Span/SpanWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility.Span;
+
+public static class SpanWriter
+{
+}
