d76927aaf712456e47c9d071c6a9c1ec02a73cd7
chrisitopherus <jochchristopher@gmail.com>
2025-04-25 21:38:21 +0200

    added keyboard watcher

---
+++ Helpers/Helpers/Utility/Keyboard/KeyData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility.Keyboard;
+
+public class KeyData
+{
+    public KeyData(ConsoleKey key, bool shift, bool alt, bool control)
+    {
+        this.Key = key;
+        this.Shift = shift;
+        this.Alt = alt;
+        this.Control = control;
+    }
+
+    public ConsoleKey Key { get; }
+
+    public bool Shift { get; }
+
+    public bool Alt { get; }
+
+    public bool Control { get; }
+}
---
+++ Helpers/Helpers/Utility/Keyboard/KeyboardWatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Utility.Keyboard;
+
+public class KeyboardWatcher
+{
+    public bool Exit { get; set; }
+
+    private bool HasModifier(ConsoleModifiers modifiers, ConsoleModifiers modifier)
+    {
+        return (modifiers & modifier) != 0;
+    }
+
+    private void WatchKeyboardWorker()
+    {
+        while (!this.Exit)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            KeyData keyData = new(
+                keyInfo.Key, 
+                this.HasModifier(keyInfo.Modifiers, ConsoleModifiers.Shift),
+                this.HasModifier(keyInfo.Modifiers, ConsoleModifiers.Alt),
+                this.HasModifier(keyInfo.Modifiers, ConsoleModifiers.Control));
+
+            // sends event
+        }
+    }
+}
