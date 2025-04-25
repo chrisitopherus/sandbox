546cd2c70a0c577f7f232ced899dbb0933f65f0e
chrisitopherus <jochchristopher@gmail.com>
2025-04-25 17:02:07 +0200

    added integer parser and long to int extension method

---
+++ Helpers/Helpers/Extension/LongExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Helpers.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Extension;
+
+public static class LongExtensions
+{
+    public static bool TryConvertToInt(this long value, out int result)
+    {
+        return IntegerParser.TryParseLongToInt(value, out result);
+    }
+
+    public static int ConvertToInt(this long value)
+    {
+        return (int)value;
+    }
+}
---
+++ Helpers/Helpers/Parsing/IntegerParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Parsing;
+
+public static class IntegerParser
+{
+    public static bool TryParseLongToInt(long value, out int result)
+    {
+        if (value is >= int.MinValue and <= int.MaxValue)
+        {
+            result = (int)value;
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+}
