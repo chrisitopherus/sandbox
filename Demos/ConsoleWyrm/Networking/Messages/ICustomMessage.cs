bc25ad9e5b472f7f6b8e5ad5bbe3e3279620fc9d
chrisitopherus <jochchristopher@gmail.com>
2025-04-28 03:44:12 +0200

    added documentation header

--- Demos/ConsoleWyrm/Networking/Messages/ICustomMessage.cs
+++ Demos/ConsoleWyrm/Networking/Messages/ICustomMessage.cs
@@ -8,6 +8,15 @@ using Network.Architecture.Interfaces;
 
 namespace ConsoleWyrm.Networking.Messages;
 
+/// <summary>
+/// Defines a custom message.
+/// </summary>
+/// <remarks>
+/// <para>Message Format:</para>
+/// <para>- (1 Byte) Type</para>
+/// <para>- (4 Bytes) Content Length (N)</para>
+/// <para>- (N Bytes) Content</para>
+/// </remarks>
 public interface ICustomMessage : IMessage
 {
     MessageType Type { get; }
