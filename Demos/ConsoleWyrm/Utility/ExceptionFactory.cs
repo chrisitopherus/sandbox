1024443c4c5c4a623b289b4357a7e8a6e811d1c8
chrisitopherus <jochchristopher@gmail.com>
2025-04-28 14:31:34 +0200

    added exception factory to have consistent exception messages

---
+++ Demos/ConsoleWyrm/Utility/ExceptionFactory.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleWyrm.Networking.Messages.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Utility;
+
+public static class ExceptionFactory
+{
+    public static InvalidDataException CreateDecodeFailure(string productName, Exception exception)
+    {
+        return CreateInvalidDataException("decode", productName, exception);
+    }
+
+    public static InvalidDataException CreateEncodeFailure(string sourceName, Exception exception)
+    {
+        return CreateInvalidDataException("encode", sourceName, exception);
+    }
+
+    public static InvalidDataException CreateInvalidFormat(string productName)
+    {
+        return new InvalidDataException($"Invalid {productName} format.");
+    }
+
+    private static InvalidDataException CreateInvalidDataException(string operation, string name, Exception? exception = default) 
+    {
+        return new InvalidDataException($"Failed to {operation} {name}", exception);
+    }
+}
