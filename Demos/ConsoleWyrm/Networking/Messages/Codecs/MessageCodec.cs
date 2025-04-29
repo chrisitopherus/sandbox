4fac7367f9f58c1855638bfd3ed496b05f94fcb3
chrisitopherus <jochchristopher@gmail.com>
2025-04-29 17:47:03 +0200

    added abstract message codec class to define message header byte count

---
+++ Demos/ConsoleWyrm/Networking/Messages/Codecs/MessageCodec.cs
@@ -0,0 +1,18 @@
+﻿using Network.Architecture.Interfaces.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Networking.Messages.Codecs;
+
+public abstract class MessageCodec<TMessage> : ISymmetricMessageCodec<TMessage>
+    where TMessage : ICustomMessage
+{
+    public byte HeaderByteCount { get; } = 5;
+
+    public abstract TMessage Decode(ReadOnlyMemory<byte> data);
+
+    public abstract ReadOnlyMemory<byte> Encode(TMessage message);
+}
