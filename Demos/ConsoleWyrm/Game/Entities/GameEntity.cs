59cc81479522e91e0e6872ad2b51e1a6ce91d285
chrisitopherus <jochchristopher@gmail.com>
2025-04-29 19:23:24 +0200

    added game entity boilerplate

--- Demos/ConsoleWyrm/Game/Entities/GameEntity.cs
+++ Demos/ConsoleWyrm/Game/Entities/GameEntity.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ConsoleWyrm.Game.Data;
+using ConsoleWyrm.Game.Graphics;
+using ConsoleWyrm.Game.Interfaces;
+using ConsoleWyrm.Game.Physics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +10,29 @@ using System.Threading.Tasks;
 
 namespace ConsoleWyrm.Game.Entities;
 
-public abstract class GameEntity
+public abstract class GameEntity : IDirty
 {
+    public GameEntity(ICollisionShape collisionShape, Sprite sprite, ConsolePosition position)
+    {
+        this.CollisionShape = collisionShape;
+        this.Sprite = sprite;
+        this.Position = position;
+    }
+
+    public bool IsDirty
+    {
+        get;
+        private set;
+    }
+
+    public ICollisionShape CollisionShape { get; }
+
+    public Sprite Sprite { get; }
+
+    public ConsolePosition Position { get; }
+
+    public void ClearDirty()
+    {
+        this.IsDirty = false;
+    }
 }
