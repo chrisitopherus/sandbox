66e53f1556733eb63b9045d7bce019cc81371222
chrisitopherus <jochchristopher@gmail.com>
2025-05-03 16:26:42 +0200

    created basic console game engine

---
+++ Game/GameStuff/Engine/ConsoleGameEngine.cs
@@ -0,0 +1,108 @@
+﻿using Helpers.Utility.Keyboard;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStuff.Engine;
+
+public class ConsoleGameEngine
+{
+    private readonly Stack<Scene> scenes = [];
+    private readonly KeyboardWatcher keyboardWatcher = new();
+    private readonly Action ressourceLoader;
+
+    public ConsoleGameEngine(Action ressourceLoader)
+    {
+        this.ressourceLoader = ressourceLoader;
+    }
+
+    public Scene? CurrentScene => this.scenes.Count > 0 ? this.scenes.Peek() : null;
+
+    public void Run()
+    {
+        this.ressourceLoader();
+        this.keyboardWatcher.KeyPressed += this.KeyboardWatcherOnKeyPressedHandler;
+        this.keyboardWatcher.Start();
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        TimeSpan lastUpdate = stopwatch.Elapsed;
+
+        while (this.scenes.Count > 0)
+        {
+            TimeSpan now = stopwatch.Elapsed;
+            TimeSpan deltaTime = now - lastUpdate;
+            lastUpdate = now;
+
+            this.CheckCollisions();
+            this.Update(deltaTime);
+            this.Render();
+
+            Thread.Sleep(16);
+        }
+
+        this.keyboardWatcher.KeyPressed -= this.KeyboardWatcherOnKeyPressedHandler;
+        this.keyboardWatcher.Stop();
+    }
+
+    public void PushScene(Scene scene)
+    {
+        scene.Init();
+        this.scenes.Push(scene);
+    }
+
+    public void PopScene()
+    {
+        this.scenes.Pop();
+    }
+
+    private void KeyboardWatcherOnKeyPressedHandler(object? sender, KeyboardWatcherKeyPressedEventArgs e)
+    {
+        foreach (Scene scene in this.scenes)
+        {
+            scene.HandleKeyInput(e.KeyData);
+
+            // if scene blocks input for lower scenes -> exit
+            if (scene.BlocksInput)
+            {
+                break;
+            }
+        }
+    }
+
+    private void CheckCollisions()
+    {
+        //
+    }
+
+    private void Update(TimeSpan deltaTime)
+    {
+        foreach (Scene scene in this.scenes)
+        {
+            scene.Update(deltaTime);
+
+            // if scene blocks updates for lower scenes -> exit
+            if (scene.BlocksUpdate)
+            {
+                break;
+            }
+        }
+    }
+
+    private void Render()
+    {
+        foreach (Scene scene in this.scenes)
+        {
+            scene.Render();
+
+            // if scene blocks renders for lower scenes -> exit
+            if (scene.BlocksRender)
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Game/GameStuff/Engine/Scene.cs
@@ -0,0 +1,26 @@
+﻿using GameStuff.Engine.Interfaces;
+using Helpers.Utility.Keyboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStuff.Engine;
+
+public abstract class Scene
+{
+    public abstract void Init();
+
+    public abstract void Update(TimeSpan deltaTime);
+
+    public abstract void Render();
+
+    public virtual void HandleKeyInput(ConsoleKeyData keyData) { }
+
+    public virtual bool BlocksUpdate { get; protected set; } = true;
+
+    public virtual bool BlocksRender { get; protected set; } = true;
+
+    public virtual bool BlocksInput { get; protected set; } = true;
+}
