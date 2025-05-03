ffa6c71b1e014b0a3a7aa3577538207b90aa3681
chrisitopherus <jochchristopher@gmail.com>
2025-05-03 16:25:52 +0200

    created console theme helper

---
+++ Demos/ConsoleWyrm/Game/Graphics/ConsoleTheme.cs
@@ -0,0 +1,14 @@
+﻿using Helpers.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWyrm.Game.Graphics;
+
+public static class ConsoleTheme
+{
+    public static ConsoleStyle DefaultStyle { get; } = new(ConsoleColor.White, ConsoleColor.Black);
+    public static ConsoleStyle HighlightStyle { get; } = new(ConsoleColor.Black, ConsoleColor.Yellow);
+}
