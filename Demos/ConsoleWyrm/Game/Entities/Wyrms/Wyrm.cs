b7b23a1cccaa28292852da88d67ba67441bd1074
chrisitopherus <jochchristopher@gmail.com>
2025-05-05 04:43:23 +0200

    implemented basic wyrm

--- Demos/ConsoleWyrm/Game/Entities/Wyrms/Wyrm.cs
+++ Demos/ConsoleWyrm/Game/Entities/Wyrms/Wyrm.cs
@@ -1,7 +1,9 @@
 ﻿using ConsoleGameEngine.Core;
 using ConsoleGameEngine.Graphics.Sprites;
 using ConsoleGameEngine.Physics.Collisions;
+using ConsoleWyrm.Game.Ressources;
 using GameStuff.Data;
+using Helpers.Utility.Keyboard;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,15 +15,27 @@ namespace ConsoleWyrm.Game.Entities.Wyrms;
 public class Wyrm : GameEntity
 {
     private readonly LinkedList<WyrmSegment> tail = [];
-    private Direction direction = Direction.Right;
     private float size = 0;
     private TimeSpan moveTimer = TimeSpan.Zero;
+    private Sprite headSprite = RessourceRegistry.GetSprite(SpriteId.WyrmHead);
+    private Sprite tailSprite = RessourceRegistry.GetSprite(SpriteId.WyrmTail);
 
     public Wyrm(Sprite sprite, ICollisionShape collisionShape, ConsolePosition position)
         : base(sprite, collisionShape, position)
     {
-        this.UpdateInterval = TimeSpan.FromMilliseconds(250);
+        this.UpdateInterval = TimeSpan.FromMilliseconds(50);
+        this.CurrentDirection = Direction.Right;
         this.IsDirty = true;
+    }
+
+    public Direction CurrentDirection { get; private set; }
+
+    public LinkedList<WyrmSegment> Tail
+    {
+        get
+        {
+            return this.tail;
+        }
     }
 
     public override void Update()
@@ -29,9 +43,28 @@ public class Wyrm : GameEntity
         this.Move();
     }
 
+    public override void HandleKeyInput(ConsoleKeyData keyData)
+    {
+        if (this.TryKeyToDirection(keyData.Key, out Direction direction))
+        {
+            this.CurrentDirection = direction;
+        }
+
+        if (keyData.Control && keyData.Key == ConsoleKey.G)
+        {
+            this.Grow();
+            this.IsDirty = true;
+        }
+    }
+
     private void Grow()
     {
-
+        ICollisionShape collisionShape = RessourceRegistry.GetCollisionShape(CollisionShapeId.Point);
+        WyrmSegment? lastSegment = this.tail.Last?.Value;
+        ConsolePosition refPos = lastSegment?.Position ?? this.Position;
+        ConsolePosition position = refPos.Add(new ConsolePosition(1, 0));
+        WyrmSegment wyrmSegment = new(this.tailSprite, collisionShape, position);
+        this.tail.AddLast(wyrmSegment);
     }
 
     private void Die()
@@ -41,6 +74,36 @@ public class Wyrm : GameEntity
 
     private void Move()
     {
+        ConsolePosition prevPos = this.Position;
+        this.Position = this.Position.MoveToDirection(this.CurrentDirection);
+        foreach (WyrmSegment wyrmSegment in this.tail)
+        {
+            (prevPos, wyrmSegment.Position) = (wyrmSegment.Position, prevPos);
+        }
 
+        this.IsDirty = true;
     }
+
+    private bool TryKeyToDirection(ConsoleKey key, out Direction direction)
+    {
+        try
+        {
+            direction = this.KeyToDirection(key);
+            return true;
+        }
+        catch
+        {
+            direction = default;
+            return false;
+        }
+    }
+
+    private Direction KeyToDirection(ConsoleKey key) => key switch
+    {
+        ConsoleKey.UpArrow => Direction.Up,
+        ConsoleKey.DownArrow => Direction.Down,
+        ConsoleKey.LeftArrow => Direction.Left,
+        ConsoleKey.RightArrow => Direction.Right,
+        _ => throw new ArgumentOutOfRangeException(nameof(key), "The specfied key is not bind to a direction."),
+    };
 }
