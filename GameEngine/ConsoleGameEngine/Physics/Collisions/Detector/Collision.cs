91bed1c8ec447783c4aac5dd5a113c60aab622d5
chrisitopherus <jochchristopher@gmail.com>
2025-05-05 14:58:26 +0200

    implemented simple collision detector with reflection

---
+++ GameEngine/ConsoleGameEngine/Physics/Collisions/Detector/Collision.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleGameEngine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Physics.Collisions.Detector;
+
+public class Collision
+{
+    public Collision(ICollidable a, ICollidable b)
+    {
+        this.A = a; 
+        this.B = b;
+    }
+
+    public ICollidable A { get; }
+    public ICollidable B { get; }
+}
