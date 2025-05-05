4b14fbc07521733d562d3380dccbefd89ffa1224
chrisitopherus <jochchristopher@gmail.com>
2025-05-05 15:00:51 +0200

    added logic for spawning and despawning

--- GameEngine/ConsoleGameEngine/Core/Scene.cs
+++ GameEngine/ConsoleGameEngine/Core/Scene.cs
@@ -8,9 +8,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleGameEngine.Core;
 
-public abstract class Scene : IInitializable, IRenderable
+public abstract class Scene : IInitializable, IRenderable, ISpawner<GameEntity>, IDespawner<GameEntity>
 {
-    public abstract IEnumerable<GameEntity> Entities { get; }
+    protected readonly List<GameEntity> gameEntities = [];
+    protected readonly Queue<GameEntity> spawnQueue = [];
+    protected readonly Queue<GameEntity> despawnQueue = [];
+
+    public IEnumerable<GameEntity> Entities
+    {
+        get
+        {
+            return this.gameEntities;
+        }
+    }
 
     public bool BlocksUpdate { get; protected set; } = true;
 
@@ -24,12 +34,26 @@ public abstract class Scene : IInitializable, IRenderable
         private set;
     }
 
+    public virtual void Spawn(GameEntity entity)
+    {
+        this.spawnQueue.Enqueue(entity);
+    }
+
+    public virtual void Despawn(GameEntity entity)
+    {
+        this.despawnQueue.Enqueue(entity);
+    }
+
     public virtual void Update(TimeSpan deltaTime)
     {
+        this.ApplySpawn();
         foreach (GameEntity entity in this.Entities)
         {
             entity.TryUpdate(deltaTime);
         }
+
+        this.CheckForDespawnRequests(this.gameEntities);
+        this.ApplyDespawn();
     }
 
     public virtual void HandleKeyInput(ConsoleKeyData keyData)
@@ -49,4 +73,44 @@ public abstract class Scene : IInitializable, IRenderable
     }
 
     protected abstract void Init();
+
+    protected virtual void HandleSpawn(GameEntity entity)
+    {
+        entity.Scene = this;
+        entity.OnSpawn();
+        this.gameEntities.Add(entity);
+    }
+
+    protected virtual void HandleDespawn(GameEntity entity)
+    {
+        entity.Scene = default;
+        entity.OnDespawn();
+        this.gameEntities.Remove(entity);
+    }
+
+    private void CheckForDespawnRequests(IEnumerable<GameEntity> entities)
+    {
+        foreach (GameEntity entity in entities.Where(e => e.IsDespawnRequested))
+        {
+            this.Despawn(entity);
+        }
+    }
+
+    private void ApplySpawn()
+    {
+        while (this.spawnQueue.Count > 0)
+        {
+            GameEntity entity = this.spawnQueue.Dequeue();
+            this.HandleSpawn(entity);
+        }
+    }
+
+    private void ApplyDespawn()
+    {
+        while (this.despawnQueue.Count > 0)
+        {
+            GameEntity entity = this.despawnQueue.Dequeue();
+            this.HandleDespawn(entity);
+        }
+    }
 }
