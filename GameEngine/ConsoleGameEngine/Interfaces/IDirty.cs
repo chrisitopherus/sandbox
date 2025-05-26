f9e727619f15092d0f37653cb08fe31df4d16e8f
chrisitopherus <jochchristopher@gmail.com>
2025-05-26 18:45:33 +0200

    added documentation

--- GameEngine/ConsoleGameEngine/Interfaces/IDirty.cs
+++ GameEngine/ConsoleGameEngine/Interfaces/IDirty.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleGameEngine.Interfaces;
 
+/// <summary>
+/// Indicates whether an object requires re-rendering or update due to changes.
+/// </summary>
 public interface IDirty
 {
+    /// <summary>
+    /// Gets a value indicating whether the object is marked as dirty.
+    /// </summary>
     bool IsDirty { get; }
 
+    /// <summary>
+    /// Clears the dirty flag, marking the object as clean.
+    /// </summary>
     void ClearDirty();
 }
