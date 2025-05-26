91c9e3fc9c141d6cd21af2ad7c02ed4cf55408ce
chrisitopherus <jochchristopher@gmail.com>
2025-05-26 18:51:03 +0200

    added multi collision shape

---
+++ GameEngine/ConsoleGameEngine/Physics/Collisions/MultiCollisionShape.cs
@@ -0,0 +1,39 @@
+﻿using GameStuff.Data;
+using Helpers.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Physics.Collisions;
+
+public class MultiCollisionShape : CollisionShape
+{
+    public MultiCollisionShape(IEnumerable<ICollisionShape> collisionShapes, ConsolePosition entityOffset)
+        : base(entityOffset)
+    {
+        Validator.NotNull(collisionShapes, nameof(collisionShapes));
+        this.CollisionShapes = collisionShapes;
+        this.RelativeCollisionCells = this.GenerateRelativeCells();
+    }
+
+    public IEnumerable<ICollisionShape> CollisionShapes { get; }
+
+    public override ConsolePosition[] RelativeCollisionCells { get; }
+
+    protected override ConsolePosition[] GenerateRelativeCells()
+    {
+        HashSet<ConsolePosition> cells = [];
+        foreach (ICollisionShape collisionShape in this.CollisionShapes)
+        {
+            foreach (ConsolePosition cellPosition in collisionShape.RelativeCollisionCells)
+            {
+                cells.Add(cellPosition);
+            }
+        }
+
+        return [..cells];
+    }
+}
