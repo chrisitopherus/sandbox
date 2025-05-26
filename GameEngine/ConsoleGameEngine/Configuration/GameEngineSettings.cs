e612d58775907f6cfccd750280a4920f57fb52a5
chrisitopherus <jochchristopher@gmail.com>
2025-05-26 18:50:11 +0200

    added console settings and game engine settings

---
+++ GameEngine/ConsoleGameEngine/Configuration/GameEngineSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Configuration;
+
+public class GameEngineSettings
+{
+    public GameEngineSettings(ConsoleSettings consoleSettings, int gameLoopIntervalMs = 16)
+    {
+        this.ConsoleSettings = consoleSettings;
+        this.GameLoopIntervalMs = gameLoopIntervalMs;
+    }
+
+    public ConsoleSettings InitialConsoleSettings { get; } = ConsoleSettings.Capture();
+
+    public ConsoleSettings ConsoleSettings { get; set; }
+
+    public int GameLoopIntervalMs { get; set; }
+}
