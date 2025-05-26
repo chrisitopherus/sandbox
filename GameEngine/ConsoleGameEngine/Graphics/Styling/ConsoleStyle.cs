4f2b4b2d2008fe74996a3a9161f36610c1f94d13
chrisitopherus <jochchristopher@gmail.com>
2025-05-26 18:50:42 +0200

    added console style

---
+++ GameEngine/ConsoleGameEngine/Graphics/Styling/ConsoleStyle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameEngine.Graphics.Styling;
+
+public readonly struct ConsoleStyle
+{
+    public ConsoleStyle(ConsoleColor foreground, ConsoleColor background)
+    {
+        this.Foreground = foreground;
+        this.Background = background;
+    }
+
+    public ConsoleColor Foreground { get; init; }
+    public ConsoleColor Background { get; init; }
+
+    public void Apply()
+    {
+        if (Console.ForegroundColor != this.Foreground)
+        {
+            Console.ForegroundColor = this.Foreground;
+        }
+
+        if (Console.BackgroundColor != this.Background)
+        {
+            Console.BackgroundColor = this.Background;
+        }
+    }
+}
